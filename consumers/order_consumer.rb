17475c8276c893a94a29290c4a855cabb2d6a282
Ryan Townsend <ryan@ryantownsend.co.uk>
2017-10-16 16:58:51 +0100

    Removed extraneous logging

--- consumers/order_consumer.rb
+++ consumers/order_consumer.rb
@@ -6,15 +6,11 @@ class OrderConsumer < Racecar::Consumer
   subscribes_to "#{ENV['KAFKA_PREFIX']}orders", start_from_beginning: true
 
   def process(message)
-    $stdout.puts "Received message: #{message.inspect}"
-
     message_value = Oj.load(message.value)
 
     order = Order.where(id: message_value.fetch('order_id')).first_or_create!({
       line_items: message_value.fetch('line_items'),
       created_at: message_value.fetch('timestamp')
     })
-
-    $stdout.puts "Created order ##{order.id}"
   end
 end
