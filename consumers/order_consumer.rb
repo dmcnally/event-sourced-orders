79845905392263e36c19f324b9414a09774a2830
Ryan Townsend <ryan@ryantownsend.co.uk>
2017-10-16 16:43:01 +0100

    Removed implementation of order consumer

--- consumers/order_consumer.rb
+++ consumers/order_consumer.rb
@@ -7,24 +7,5 @@ class OrderConsumer < Racecar::Consumer
 
   def process(message)
     $stdout.puts "Received message: #{message.inspect}"
-
-    begin
-      message_value = Oj.load(message.value)
-    rescue Exception => ex
-      $stdout.puts ex.message
-      $stdout.puts ex.backtrace
-      return true
-    end
-
-    order = Order.where(id: message_value.fetch('order_id')).first_or_create!({
-      line_items: message_value.fetch('line_items'),
-      created_at: message_value.fetch('timestamp')
-    })
-
-    $stdout.puts "Created order ##{order.id}"
-  rescue Exception => ex
-    $stdout.puts ex.message
-    $stdout.puts ex.backtrace
-    raise
   end
 end
