a69d91a6aa079bbe567adde43afb805c4a504cc0
Ryan Townsend <ryan@ryantownsend.co.uk>
2017-11-05 07:03:47 +0000

    Delete orders when migrating

--- db/migrate/20171016170300_add_total_to_orders.rb
+++ db/migrate/20171016170300_add_total_to_orders.rb
@@ -1,5 +1,7 @@
 class AddTotalToOrders < ActiveRecord::Migration[5.1]
   def change
+    Order.delete_all
+
     change_table :orders, id: :uuid, default: nil do |t|
       t.integer :total, null: false
     end
