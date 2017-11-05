0aaf3ce89e8a10cb25784b896dc199817667d0a3
Ryan Townsend <ryan@ryantownsend.co.uk>
2017-11-05 07:21:37 +0000

    Symbolized keys when reading JSON

--- consumers/order_consumer.rb
+++ consumers/order_consumer.rb
@@ -6,16 +6,16 @@ class OrderConsumer < Racecar::Consumer
   subscribes_to "#{ENV['KAFKA_PREFIX']}orders", start_from_beginning: true
 
   def process(message)
-    message_value = Oj.load(message.value)
+    message_value = Oj.load(message.value, symbol_keys: true)
 
     # find our order, or initialize a new one
-    order = Order.where(id: message_value.fetch('order_id')).first_or_initialize
+    order = Order.where(id: message_value.fetch(:order_id)).first_or_initialize
 
     # add our attributes
-    order.line_items = message_value.fetch('line_items')
-    order.created_at = message_value.fetch('timestamp')
-    order.total = message_value.fetch('line_items').map { |li|
-      Integer(li.fetch('price')) * Integer(li.fetch('quantity'))
+    order.line_items = message_value.fetch(:line_items)
+    order.created_at = message_value.fetch(:timestamp)
+    order.total = message_value.fetch(:line_items).map { |li|
+      Integer(li.fetch(:price)) * Integer(li.fetch(:quantity))
     }.reduce(:+)
 
     # persist the order
