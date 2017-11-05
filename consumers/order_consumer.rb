a67e84d6c191265823241c4be79a58a15c207884
Ryan Townsend <ryan@ryantownsend.co.uk>
2017-11-05 07:17:52 +0000

    Cater for missing keys

--- consumers/order_consumer.rb
+++ consumers/order_consumer.rb
@@ -8,12 +8,19 @@ class OrderConsumer < Racecar::Consumer
   def process(message)
     message_value = Oj.load(message.value)
 
-    order = Order.where(id: message_value.fetch('order_id')).first_or_create!({
-      line_items: message_value.fetch('line_items'),
-      created_at: message_value.fetch('timestamp'),
-      total: message_value.fetch('line_items').map { |li|
-        Integer(li['price']) * Integer(li['quantity'])
-      }.reduce(:+)
-    })
+    # find our order, or initialize a new one
+    order = Order.where(id: message_value.fetch('order_id')).first_or_initialize
+
+    # add our attributes
+    order.line_items = message_value.fetch('line_items')
+    order.created_at = message_value.fetch('timestamp')
+    order.total = message_value.fetch('line_items').map { |li|
+      Integer(li.fetch('price')) * Integer(li.fetch('quantity'))
+    }.reduce(:+)
+
+    # persist the order
+    order.save!
+  rescue KeyError => e
+    raise KeyError, "#{e.message}\nMessage: #{message_value.inspect}"
   end
 end
